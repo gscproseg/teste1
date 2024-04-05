c5427137aa594bf5a30e24427a3be9ffa2bcf9dc
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-04 23:33:08 -0300

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -1,22 +1,24 @@
-import streamlit as st 
+import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
 from yolo_predictions import YOLO_Pred
 
-# load yolo model
+# Carrega o modelo YOLO
 yolo = YOLO_Pred('./models/best.onnx',
                  './models/data.yaml')
 
-
 def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    # any operation 
-    #flipped = img[::-1,:,:]
-    pred_img = yolo.predictions(img)
-
+    img_vid = frame.to_ndarray(format="bgr24")
+    # Qualquer operação adicional que você desejar
+    # Por exemplo, inverter a imagem:
+    # flipped = img_vid[::-1,:,:]
+    
+    # Realiza previsões com o modelo YOLO
+    pred_img = yolo.predictions(img_vid)
+    
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
-
-webrtc_streamer(key="example", 
+# Inicia o aplicativo Streamlit com o fluxo de vídeo
+webrtc_streamer(key="cam",
                 video_frame_callback=video_frame_callback,
-                media_stream_constraints={"video":True,"audio":False})
+                media_stream_constraints={"video": True, "audio": False})
