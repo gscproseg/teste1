bc9d44482d17a66b2a384d82658519995128990c
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-04 23:36:58 -0300

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -4,21 +4,23 @@ import av
 from yolo_predictions import YOLO_Pred
 
 # Carrega o modelo YOLO
-yolo = YOLO_Pred('./models/best.onnx',
-                 './models/data.yaml')
+yolo = YOLO_Pred('./models/best.onnx', './models/data.yaml')
 
 def video_frame_callback(frame):
-    img_vid = frame.to_ndarray(format="bgr24")
-    # Qualquer operação adicional que você desejar
-    # Por exemplo, inverter a imagem:
-    # flipped = img_vid[::-1,:,:]
-    
-    # Realiza previsões com o modelo YOLO
-    pred_img = yolo.predictions(img_vid)
-    
-    return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+    try:
+        img_vid = frame.to_ndarray(format="bgr24")
+        
+        # Realiza previsões com o modelo YOLO
+        pred_img = yolo.predictions(img_vid)
+        
+        return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+    except Exception as e:
+        st.error(f"Erro ao processar o frame: {e}")
 
 # Inicia o aplicativo Streamlit com o fluxo de vídeo
-webrtc_streamer(key="cam",
-                video_frame_callback=video_frame_callback,
-                media_stream_constraints={"video": True, "audio": False})
+try:
+    webrtc_streamer(key="cam",
+                    video_frame_callback=video_frame_callback,
+                    media_stream_constraints={"video": True, "audio": False})
+except Exception as e:
+    st.error(f"Erro ao iniciar o stream: {e}")
