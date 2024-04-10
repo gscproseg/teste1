ff804b54d7b02fbc1ec57773e7c8239b8f7391b8
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-10 00:24:35 -0300

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -1,26 +1,46 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+
+import streamlit as st 
+from streamlit_webrtc import (
+    VideoProcessorBase,
+    RTCConfiguration,
+    webrtc_streamer
+)
 import av
 from yolo_predictions import YOLO_Pred
 
-# Carrega o modelo YOLO
-yolo = YOLO_Pred('./models/best.onnx', './models/data.yaml')
+# Carregar o modelo YOLO
+yolocam = YOLO_Pred(onnx_model='./best.onnx', data_yaml='./data.yaml')
 
-def video_frame_callback(frame):
-    try:
-        img_vid = frame.to_ndarray(format="bgr24")
-        
-        # Realiza previsões com o modelo YOLO
-        pred_img = yolo.predictions(img_vid)
-        
-        return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
-    except Exception as e:
-        st.error(f"Erro ao processar o frame: {e}")
+# Definir configuração RTC (WebRTC)
+rtc_configuration = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
-# Inicia o aplicativo Streamlit com o fluxo de vídeo
-try:
-    webrtc_streamer(key="cam",
-                    video_frame_callback=video_frame_callback,
-                    media_stream_constraints={"video": True, "audio": False})
-except Exception as e:
-    st.error(f"Erro ao iniciar o stream: {e}")
+class YOLOVideoProcessor(VideoProcessorBase):
+    async def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        try:
+            img_cam = frame.to_ndarray(format="bgr24")
+            pred_img_video = yolocam.predictions(img_cam)
+
+            # Adicionar mensagem de log para verificar as previsões
+            st.write(f"Previsões: {pred_img_video}")
+
+            return av.VideoFrame.from_ndarray(pred_img_video, format="bgr24")
+        except Exception as e:
+            st.error(f"Erro durante o processamento do frame: {e}")
+            return frame
+
+# Configurar e iniciar a transmissão WebRTC
+webrtc_ctx = webrtc_streamer(
+    key="example",
+    video_processor_factory=YOLOVideoProcessor,
+    rtc_configuration=rtc_configuration,
+    async_processing=True,
+    media_stream_constraints={"video": True, "audio": False},
+)
+
+# Exibir a interface do Streamlit
+if webrtc_ctx.state == "connected":
+    st.write("Streaming de vídeo com detecção de objetos está ativo.")
+else:
+    st.write("Aguardando a transmissão de vídeo começar...")
