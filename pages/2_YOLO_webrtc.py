ad3a04134d5df32f61c104a224262494574bba4b
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-04-10 01:25:18 -0300

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -12,10 +12,6 @@ from yolo_predictions import YOLO_Pred
 # Carregar o modelo YOLO
 yolocam = YOLO_Pred(onnx_model='./models/best.onnx', data_yaml='./models/data.yaml')
 
-# Iniciar loop de eventos asyncio
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
 # Definir configuração RTC (WebRTC)
 rtc_configuration = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -27,7 +23,9 @@ class YOLOVideoProcessor(VideoProcessorBase):
             img_cam = frame.to_ndarray(format="bgr24")
 
             # Processamento assíncrono
-            pred_img_video = await loop.run_in_executor(None, yolocam.predictions, img_cam)
+            pred_img_video = await asyncio.get_event_loop().run_in_executor(
+                None, yolocam.predictions, img_cam
+            )
 
             # Adicionar mensagem de log para verificar as previsões
             st.write(f"Previsões: {pred_img_video}")
@@ -37,21 +35,26 @@ class YOLOVideoProcessor(VideoProcessorBase):
             st.error(f"Erro durante o processamento do frame: {e}")
             return frame
 
-# Configurar e iniciar a transmissão WebRTC
-webrtc_ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=YOLOVideoProcessor,
-    rtc_configuration=rtc_configuration,
-    async_processing=True,
-    media_stream_constraints={"video": True, "audio": False},
-)
+async def main():
+    # Configurar e iniciar a transmissão WebRTC
+    webrtc_ctx = webrtc_streamer(
+        key="example",
+        video_processor_factory=YOLOVideoProcessor,
+        rtc_configuration=rtc_configuration,
+        async_processing=True,
+        media_stream_constraints={"video": True, "audio": False},
+    )
 
-# Loop principal para atualizar a interface
-while True:
-    if webrtc_ctx.state.playing:
-        st.write("Streaming de vídeo com detecção de objetos está ativo.")
-    else:
-        st.write("Aguardando a transmissão de vídeo começar...")
+    # Loop principal para atualizar a interface
+    while True:
+        if webrtc_ctx.state.playing:
+            st.write("Streaming de vídeo com detecção de objetos está ativo.")
+        else:
+            st.write("Aguardando a transmissão de vídeo começar...")
 
-    # Atualizar a interface de forma assíncrona
-    await asyncio.sleep(0)
+        # Atualizar a interface de forma assíncrona
+        await asyncio.sleep(0)
+
+# Executar a função principal assíncrona
+if __name__ == "__main__":
+    asyncio.run(main())
